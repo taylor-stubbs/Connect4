ec9236e9ffe975e7f818a771354e08c05f09a58f
Taylor Stubbs <taylor.stubbs@infinityworks.com>
2020-09-25 11:12:45 +0100

    Added file save feature'
    ;
    '

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,16 @@
 const { isModuleSpecifier } = require('@babel/types');
+const { json } = require('express');
 const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 
 app.use(express.static('./Source'));
 app.use(express.json());
 
+
 class Player {
+
   name;
   colour;
   score;
@@ -34,43 +38,55 @@ class Player {
   }
 }
 
+
 const game = {
-    players: [],
-    board: [],
-    turn: 0,
-    winner: null,
+  players: [],
+  board: [],
+  turn: 0,
+  winner: null,
+}
+
+async function saveGame(players) {
+  await fs.writeFile(__dirname + '/data/players.json', '{\n' + '"p1":' + JSON.stringify(players[0]) + ',\n"p2":' + JSON.stringify(players[1]) + '\n}');
+}
+
+async function loadGame() {
+  const players = await fs.readFile(__dirname + '/data/players.json').then(s => JSON.parse(s));
+  const p1 = new Player(players.p1.name, players.p1.colour, players.p1.score);
+  const p2 = new Player(players.p2.name, players.p2.colour, players.p2.score);
+  return [p1,p2];
 }
 
 function createBoard(row, col) {
-    const boardArray = [];
-    for (let i = 0; i < col; i += 1) {
-        const colArray = [];
-        for (let j = 0; j < row; j += 1) {
-            colArray.push(null);
-        }
-        boardArray.push(colArray);
-    }
-    return boardArray;
+  const boardArray = [];
+  for (let i = 0; i < col; i += 1) {
+      const colArray = [];
+      for (let j = 0; j < row; j += 1) {
+          colArray.push(null);
+      }
+      boardArray.push(colArray);
+  }
+  return boardArray;
 }
 
 function switchPlayer(turn) {
-    return Math.abs(turn - 1);
+  return Math.abs(turn - 1);
 }
 
 function placeCounter(col, board, player) {
-    let newBoard = board
-    let rowPointer = board[col].length - 1;
-    for (rowPointer; rowPointer >= 0; rowPointer -= 1) {
-        if (board[col][rowPointer] === null) { // If current position is empty place counter
-          newBoard[col][rowPointer] = player.colour;
-          break;
-        }
+  let newBoard = board;
+  let rowPointer = board[col].length - 1;
+  for (rowPointer; rowPointer >= 0; rowPointer -= 1) {
+      if (board[col][rowPointer] === null) { // If current position is empty place counter
+        newBoard[col][rowPointer] = player.colour;
+        break;
       }
-    return newBoard;
+  }
+  return newBoard;
 }
 
 function validMove(col, board) {
-    return board[col].includes(null);
+  return board[col].includes(null);
 }
 
 function checkWin(row, col, board, player) {
@@ -86,7 +102,8 @@ function checkWin(row, col, board, player) {
       count = 0;
     }
     if (count >= 4) {
-      win = true;
+      player.addWin();
+      return player;
     }
   }
 
@@ -100,18 +117,77 @@ function checkWin(row, col, board, player) {
       count = 0;
     }
     if (count >= 4) {
-      win = true;
+      player.addWin();
+      return player;
     }
   }
-  if (win) {
-    player.addWin();  
+
+  if (posDiagonalWin(row, col, board, player) || negDiagonalWin(row, col, board, player)) {
+    player.addWin();
     return player;
   }
+  
   return null;
 }
 
+function negDiagonalWin(row, col, board, player) {
+  const h = board[0].length;
+  const w = board.length;
+  var count = 0;
+
+  var rp = row; // Row Pointer
+  var cp = col; // Col Pointer
+
+  while(rp < h - 1 && cp < w - 1) {
+    rp += 1;
+    cp += 1;
+  }
+
+  while(rp > 0 && cp > 0) {
+    if (board[cp][rp] === player.colour) {
+      count += 1;
+    } else {
+      count = 0;
+    }
+    if (count >= 4) {
+      return true;
+    }
+    rp -= 1;
+    cp -= 1;
+  }
+  return false;
+}
+
+function posDiagonalWin(row, col, board, player) {
+  const h = board[0].length;
+  const w = board.length;
+  var count = 0;
+
+  var rp = row; // Row Pointer
+  var cp = col; // Col Pointer
+
+  while(rp < h - 1 && cp > 0) {
+    rp += 1;
+    cp -= 1;
+  }
+
+  while(rp > 0 && cp < w - 1) {
+    if (board[cp][rp] === player.colour) {
+      count += 1;
+    } else {
+      count = 0;
+    }
+    if (count >= 4) {
+      return true;
+    }
+    rp -= 1;
+    cp += 1;
+  }
+  return false;
+}
+
 function getRowCoord(col, board) {
-  let rowPointer = board[col].length - 1
+  let rowPointer = (board[col]).length - 1
   for (rowPointer; rowPointer >= 0; rowPointer -= 1) {
       if (board[col][rowPointer] == null) {
           return rowPointer;
@@ -120,50 +196,62 @@ function getRowCoord(col, board) {
   return null;
 }
 
-app.post('/board', (req, res) => {
-    let player1 = new Player('Taylor', 'red');
-    let player2 = new Player('Ryan', 'yellow');
-    const rowInput = req.body.row;
-    const colInput = req.body.col;
-    if (rowInput > 10 || colInput > 12) {
-        res.sendStatus(400);
-    }
 
-    // Maybe make this into a seperate function 
-    game.players.push(player1,player2);
-    game.board = createBoard(rowInput, colInput);
-    game.turn = 0;
-    game.winner = null;
-    res.send(game);
+app.post('/board', (req, res) => {
+  
+  const rowInput = req.body.row;
+  const colInput = req.body.col;
+  if (rowInput > 10 || colInput > 12) {
+      res.sendStatus(400);
+  }
+
+  // Maybe make this into a seperate function 
+  
+  game.board = createBoard(rowInput, colInput);
+  game.turn = 0;  
+  game.winner = null;
+  res.send(game);
 });
 
 
 app.post('/counter', (req, res) => {
-    const col = req.body.col;
-    const row = getRowCoord(col, game.board);
+  const col = req.body.col;
+  const row = getRowCoord(col, game.board);
 
-    if (validMove(col, game.board) && game.winner === null) {
-        game.board = placeCounter(req.body.col, game.board, (game.players[game.turn]));
-        game.winner = checkWin(row, col, game.board, game.players[game.turn]);
-        game.turn = switchPlayer(game.turn);
-        res.send(game);
-    } else {
-      res.sendStatus(400);
-    }
+  if (validMove(col, game.board) && game.winner === null) {
+      game.board = placeCounter(req.body.col, game.board, (game.players[game.turn]));
+      game.winner = checkWin(row, col, game.board, game.players[game.turn]);
+      game.turn = switchPlayer(game.turn);
+      saveGame(game.players);
+      res.send(game);
+  } else {
+    res.sendStatus(400);
+  }
 });
 
+
+
 if (process.env.NODE_ENV !== "test") {
-    app.listen(8080, () => {
-        console.log('server started on port 8080');
-    });
+  app.listen(8080, async () => {
+    try{
+      players = await loadGame();
+      player1 = players[0];
+      player2 = players[1];
+    } catch(err) {
+      player1 = new Player('Taylor', 'red', 0);
+      player2 = new Player('Ryan', 'yellow', 0);
+    }
+    game.players.push(player1,player2);
+    console.log('server started on port 8080');
+  });
 };
 
 module.exports = {
-    placeCounter,
-    createBoard,
-    switchPlayer,
-    checkWin,
-    app,
-    Player,
-    getRowCoord
+  placeCounter,
+  createBoard,
+  switchPlayer,
+  checkWin,
+  app,
+  Player,
+  getRowCoord
 }
--- Source/index.js
+++ Source/index.js
@@ -1,68 +1,70 @@
-function render(board, player) {
-    const rows = board[0].length;
-    const cols = board.length;
-    $('#select-row').empty();
-    $('#game-board').empty();
-    for (let i = 0; i < cols; i += 1) {
-        $('#game-board').append(`<div id="${i}" class="column"></div>`);
-        $(`#game-board #${i}`).on('click', () => {
-          makeMove(i);
-        });
-        $(`#${i}`).hover(function () {
-            $(`#select-row #${i}`).css('visibility', 'visible');
-        }, function () {
-            $(`#select-row #${i}`).css('visibility', 'hidden');
-        });
-        for (let j = 0; j < rows; j += 1) {
-          $(`#${i}`).append(`<div id="${i}-${j}" class="row"></div>`);
-          $(`#${i}-${j}`).css('background-color', board[i][j]);
-        };
-        $('#select-row').append(`<div id="${i}" class="row"></div>`);
-        $('#select-row').children().css('background-color', player.colour);
-    }
-}
-
-
 function makeMove(colChoice) {
-    const body = {
-        col: colChoice
-    };
-    $.ajax({
-        type: 'POST',
-        url: '/counter',
-        data: JSON.stringify(body),
-        contentType: 'application/json',
-        success: result => {
-            render(result.board, result.players[result.turn]);
-            if(result.winner !== null) {
-                $('#win-banner').text(`${result.winner.name} wins!`);
-                $('#win-banner').css('visibility', 'visible');
-                $(`#${result.winner.colour}-score .score-label`).text(result.winner.score);
-            }
-        },
-    });
+  const body = {
+    col: colChoice
+  };
+  $.ajax({
+    type: 'POST',
+    url: '/counter',
+    data: JSON.stringify(body),
+    contentType: 'application/json',
+    success: result => {
+      render(result.board, result.players[result.turn]);
+      if (result.winner !== null) {
+        $('#win-banner').text(`${result.winner.name} wins!`);
+        $('#win-banner').css('visibility', 'visible');
+        $(`#${result.winner.colour}-score .score-label`).text(result.winner.score);
+      }
+    },
+  });
 }
+
+function render(board, player) {
+  const rows = board[0].length;
+  const cols = board.length;
+  $('#select-row').empty();
+  $('#game-board').empty();
+  for (let i = 0; i < cols; i += 1) {
+    $('#game-board').append(`<div id="${i}" class="column"></div>`);
+    $(`#game-board #${i}`).on('click', () => {
+      makeMove(i);
+    });
+    $(`#${i}`).hover(function () {
+      $(`#select-row #${i}`).css('visibility', 'visible');
+    }, function () {
+        $(`#select-row #${i}`).css('visibility', 'hidden');
+    });
+    for (let j = 0; j < rows; j += 1) {
+      $(`#${i}`).append(`<div id="${i}-${j}" class="row"></div>`);
+      $(`#${i}-${j}`).css('background-color', board[i][j]);
+    };
+    $('#select-row').append(`<div id="${i}" class="row"></div>`);
+    $('#select-row').children().css('background-color', player.colour);
+  }
+}
+
+
+
 
 function newGame() {
-    $('#win-banner').css('visibility', 'hidden');
+  $('#win-banner').css('visibility', 'hidden');
 
-    const body = {
-        col: $('#col-input').val(),
-        row: $('#row-input').val(),
-    }
-    $.ajax({
-        type: 'POST',
-        url: '/board',
-        data: JSON.stringify(body),
-        contentType: 'application/json',
-        success: result => {
-            render(result.board, result.players[result.turn]);
-            $('#game-board').css('visibility', 'visible');
-        },
-        error: function () {
-            window.alert("Invalid board size. Must be no larger than 10x12")
-        },
-    });
+  const body = {
+    col: $('#col-input').val(),
+    row: $('#row-input').val(),
+  }
+  $.ajax({
+    type: 'POST',
+    url: '/board',
+    data: JSON.stringify(body),
+    contentType: 'application/json',
+    success: result => {
+        render(result.board, result.players[result.turn]);
+        $('#game-board').css('visibility', 'visible');
+    },
+    error: function () {
+        window.alert("Invalid board size. Must be no larger than 10x12")
+    },
+  });
 }
 
 
